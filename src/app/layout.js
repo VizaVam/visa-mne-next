b5b7a38e7b889083ce4384f0e97dfb32b4e581a1
ana <yosip19h@gmail.com>
2025-02-09 19:55:42 +0500

    papu eb

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Header from '../components/header'
 import Footer from '../components/footer';
 import Modal from "@/components/modal";
 import {ModalProvider} from "@/components/modalcontext";
+import Head from "next/head";
 
 const interSans = Inter({
     variable: '--font-inter-sans',
@@ -21,6 +22,9 @@ export const metadata = {
     title: "Открыть визу в Минске – компания VISA VAM",
     keywords: "планирование путешествия в США и Европе, отдых, отдых в Европе и США, путешествие",
     robots: "index, follow",
+    icons: {
+        icon: "/favicon.ico", // ✅ This sets the favicon properly
+    }
 };
 
 export default function RootLayout({children}) {

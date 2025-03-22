df3da15fdb999cf759adbcff48aa7f621b8b8748
ana <yosip19h@gmail.com>
2025-03-22 08:47:43 +0500

    robot1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     siteUrl: 'https://visavam.by', // Укажи свой домен
-    generateRobotsTxt: true, // Создаст robots.txt автоматически
     changefreq: 'daily', // Частота обновления страниц
     priority: 1.0, // Приоритет главной страницы
     sitemapSize: 19, // Макс. страниц в одном файле

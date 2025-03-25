d56b9c3759a266dd66aa19d304716f1d84760db8
ana <yosip19h@gmail.com>
2025-03-25 07:11:36 +0500

    contactMic

--- src/app/kontakty/page.jsx
+++ src/app/kontakty/page.jsx
@@ -2,27 +2,6 @@ import Breadcrumbs from "@/components/Breadcrumbs";
 
 export { metadata } from "./metadata"
 import ContactsPage from "@/components/contactsPage";
-import Script from "next/script";
-
-export const structuredData = {
-    "@context": "https://schema.org",
-    "@type": "LocalBusiness",
-    "name": "VISA VAM",
-    "image": "https://visamne.vercel.app/new-logo.svg",
-    "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "пр. Победителей 17, офис 1204",
-        "addressLocality": "Минск",
-        "addressRegion": "Минская область",
-        "postalCode": "220004",
-        "addressCountry": "BY"
-    },
-    "telephone": "+375 29 68 00 620",
-    "email": "info@visavam.by",
-    "openingHours": "Пн-пт: 09:00-19:00\n" +
-        "\n" +
-        "Сб: 10:00-14:00"
-};
 
 export default function Page() {
 
@@ -33,11 +12,6 @@ export default function Page() {
 
     return (
         <>
-            <Script
-                type="application/ld+json"
-                strategy="beforeInteractive"
-                dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-            />
             <Breadcrumbs breadcrumbs={breadcrumbs} />
             <ContactsPage />
         </>

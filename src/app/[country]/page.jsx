8832527182d18b5e6635a5b1105d257f1b2740e4
ana <yosip19h@gmail.com>
2025-04-17 19:01:45 +0500

    metaaaa2

--- src/app/[country]/page.jsx
+++ src/app/[country]/page.jsx
@@ -21,6 +21,7 @@ const countries = [
 ];
 
 export async function generateStaticParams() {
+    console.log("Generating static params:", countries);
     return countries.map((country) => ({
         country: country.url,
     }));

a8207853dbff3951a2d0560f797fdef5a58c3e7d
ana <yosip19h@gmail.com>
2025-04-19 22:44:23 +0500

    notFound111

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,6 @@
+import { notFound } from 'next/navigation';
+
 export default function NotFound() {
-    return (
-        <div className={"h-dvh flex flex-col items-center justify-center px-[7%]"}>
-            <h1 className={"lg:text-[180px] md:text-[160px] sm:text-[140px] mdd:text-[130px] font-bold"}>404</h1>
-            <div className={"flex flex-col items-center justify-center gap-1"}>
-                <p className={"text-2xl mdd:text-xl font-medium"}>Здесь ничего нет...</p>
-                <p className={"text-xl mdd:text-[16px] font-medium"}>Возможно страница была удалена или перенесена на другой адрес</p>
-            </div>
-            <a href={"/"} className={"mt-8 mdd:w-full"}>
-                <button className={"bg-[#15419E] rounded-[2px] py-4 px-8 text-white mdd:w-full"}>
-                    Вернуться на главную
-                </button>
-            </a>
-        </div>
-    );
+    notFound(); // Это установит статус 404
+    return null; // Этот код не будет достигнут, но нужен для синтаксиса
 }

889ea65fdca54350b86f480385db2a3f19d77e88
ana <yosip19h@gmail.com>
2025-04-29 08:26:25 +0500

    chunk11

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 const nextConfig = {
     // Включить сжатие gzip/brotli
     compress: true,
+    experimental: {
+        // Подгружает только необходимые полифиллы
+        modern: true,
+    },
 };
 module.exports = nextConfig;

0ff78050920f89c0186f8dfa84ae2451dece1abf
ana <yosip19h@gmail.com>
2025-04-29 07:28:29 +0500

    chunk2

--- next.config.js
+++ next.config.js
@@ -21,15 +21,6 @@ const nextConfig = {
                     },
                 ],
             },
-            {
-                source: '/_next/static/chunks/polyfills-*.js',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                ],
-            },
         ];
     },};
 module.exports = nextConfig;

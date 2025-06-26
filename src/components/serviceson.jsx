8104637c5b829f98408438094cfc959ad38e6abd
ana <yosip19h@gmail.com>
2025-06-26 09:51:32 +0500

    priceTableAnotherC

--- src/components/serviceson.jsx
+++ src/components/serviceson.jsx
@@ -24,21 +24,51 @@ export const otherCountries = [
             "оплата пошлины;",
             "личное собеседование с Консулом на английском или русском языке."],
         typevc: "Типы виз в США",
-        typevbc: ["Туристическая","Гостевая","Условия","Бизнес-виза","Студенческая","Визы членов экипажей", "Виза невесты/визы жены"],
-        typevlc: ["asd", "asd", "asd","asd","asd","asd","asd"],
+        typevbc: ["Туристическая", "Гостевая", "Условия", "Бизнес-виза", "Студенческая", "Визы членов экипажей", "Виза невесты/визы жены"],
+        typevlc: ["asd", "asd", "asd", "asd", "asd", "asd", "asd"],
         enabled: [false, false, false, false, false, false, false],
         priceGood: 0,
         priceTitle: "Наши услуги",
         priceVariants: [
-            <span>Ваши документы-основания для получения визы - <span className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px]">400</span> бел. руб.</span>,
-            <span>Сопровождение под ключ - <span className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px]">2800</span> бел. руб.</span>,
+            <span>Ваши документы-основания для получения визы - <span
+                className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px]">400</span> бел. руб.</span>,
+            <span>Сопровождение под ключ - <span
+                className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px]">2800</span> бел. руб.</span>,
         ],
         priceTable: [
-            { service: "Услуга компании", cost: <span><span
-                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">400</span> бел. руб.</span>, note: "Консультативная работа, помощь в подготовке необходимого пакета документов (включая перевод), запись на подачу визового заявления" },
-            { service: "Консульский/сервисный/логистический сбор", cost: <span
-                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">$195</span>, note: "Оплачивается лично в Консульстве США при подаче" },
-            { service: "Медицинская страховка", cost: "Оплачивается отдельно", note: "Поможем подобрать оптимальный вариант!" }
+            {
+                service: "Услуга компании",
+                cost: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">400</span> бел. руб.</span>,
+                note: "Консультативная работа, помощь в подготовке необходимого пакета документов (включая перевод), запись на подачу визового заявления"
+                ,
+                serviceMob: "Консультация, подготовка документов (включая перевод), запись на подачу заявления;\n" +
+                    "\n",
+                costMob: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">400</span> бел. руб.</span>
+                ,
+            },
+            {
+                service: "Консульский/сервисный/логистический сбор",
+                cost: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">195</span> $</span>,
+                note: "Оплачивается лично в Консульстве США при подаче"
+                ,
+                serviceMob: "Консульский сбор\n" +
+                    "\n",
+                costMob: <span>$<span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">195</span> (оплачивается лично в Консульстве США при подаче)</span>
+                ,
+            },
+            {
+                service: "Медицинская страховка",
+                cost: "Оплачивается отдельно",
+                note: "Поможем подобрать оптимальный вариант!"
+                ,
+                serviceMob: "Медицинская страховка\n" +
+                    "\n",
+                costMob: "Оплачивается отдельно: подберем оптимальный вариант!!!",
+            }
         ]
     },
     {
@@ -54,7 +84,7 @@ export const otherCountries = [
         textTop: "Виза для посещения Великобритании большим спросом среди жителей Беларуси.Она дает право находиться на территории Соединенного Королевства **6 месяцев, 2 года или 5 лет** - это зависит от размера консульского сбора, который Вы готовы заплатить.",
         text1: "Наша компания помогает в оформлении необходимого пакета документов. В стоимость услуги включен перевод всех необходимых для подачи заявления документов на английский язык. Все документы собираются заранее.",
         typevc: "Типы виз в Великобританию",
-        typevbc: ["Туристическая","Деловая","Гостевая"],
+        typevbc: ["Туристическая", "Деловая", "Гостевая"],
         typevlc: ["asd", "asd", "asd"],
         enabled: [false, false, false],
         text7: "**Важно!** Документы находятся на рассмотрении от 8 недель.",
@@ -62,10 +92,38 @@ export const otherCountries = [
         priceGood: 1,
         price1: "400",
         priceTable: [
-            { service: "Услуга компании", cost: <span><span
-                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">400</span> бел. руб.</span>, note: "Консультативная работа, помощь в подготовке необходимого пакета документов (включая перевод), запись на подачу визового заявления" },
-            { service: "Консульский/сервисный/логистический сбор", cost: "Оплачивается отдельно", note: "Оплачивается лично в консульстве или ВЦ при подаче" },
-            { service: "Медицинская страховка", cost: "Оплачивается отдельно", note: "Поможем подобрать оптимальный вариант!" }
+            {
+                service: "Услуга компании",
+                cost: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">400</span> бел. руб.</span>,
+                note: "Консультативная работа, помощь в подготовке необходимого пакета документов (включая перевод), запись на подачу визового заявления"
+                ,
+                serviceMob: "Консультация, подготовка документов (включая перевод), запись на подачу заявления;\n" +
+                    "\n",
+                costMob: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">400</span> бел. руб.</span>
+                ,
+            },
+            {
+                service: "Консульский/сервисный/логистический сбор",
+                cost: "Оплачивается отдельно",
+                note: "Оплачивается лично в консульстве или ВЦ при подаче"
+                ,
+                serviceMob: "Консульский/сервисный/логистический сбор\n" +
+                    "\n",
+                costMob: "Оплачивается отдельно в консульстве или ВЦ при подаче\n" +
+                    "\n",
+            },
+            {
+                service: "Медицинская страховка",
+                cost: "Оплачивается отдельно",
+                note: "Поможем подобрать оптимальный вариант!"
+                ,
+                serviceMob: "Медицинская страховка\n" +
+                    "\n",
+                costMob: "Оплачивается отдельно: подберем оптимальный вариант!!!\n" +
+                    "\n",
+            }
         ]
     },
     {
@@ -93,13 +151,45 @@ export const otherCountries = [
         priceGood: 0,
         priceTitle: "Наши услуги",
         priceVariants: [
-            <span>Подготовка полного пакета документов и запись для подачи визового заявления - <span className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px]">690</span> бел. руб.</span>,
+            <span>Подготовка полного пакета документов и запись для подачи визового заявления - <span
+                className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px]">690</span> бел. руб.</span>,
         ],
         priceTable: [
-            { service: "Деловая виза (услуга компании)", cost: <span><span
-                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">690</span> бел. руб.</span>, note: "Консультативная работа, помощь в подготовке необходимого пакета документов (включая перевод), запись на подачу визового заявления" },
-            { service: "Консульский/сервисный/логистический сбор", cost: "Оплачивается отдельно", note: "Оплачивается лично в Консульстве КНР при подаче" },
-            { service: "Медицинская страховка", cost: "Оплачивается отдельно", note: "Поможем подобрать оптимальный вариант!" }
+
+
+            {
+                service: "Деловая виза (услуга компании)",
+                cost: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">690</span> бел. руб.</span>,
+                note: "Консультативная работа, помощь в подготовке необходимого пакета документов (включая перевод), запись на подачу визового заявления"
+                ,
+                serviceMob: "Деловая виза: консультация, подготовка документов (включая перевод), запись на подачу заявления\n" +
+                    "\n",
+                costMob: <span><span
+                    className="text-orange-500 md:text-[20px] sm:text-[18px] mdd:text-[18px] whitespace-nowrap font-medium">690</span> бел. руб.</span>
+                ,
+            },
+            {
+                service: "Консульский/сервисный/логистический сбор",
+                cost: "Оплачивается отдельно",
+                note: "Оплачивается лично в Консульстве КНР при подаче"
+                ,
+                serviceMob: "Консульский/сервисный/логистический сбор\n" +
+                    "\n",
+                costMob: "Оплачивается лично в Консульстве КНР при подаче\n" +
+                    "\n",
+            },
+            {
+                service: "Медицинская страховка",
+                cost: "Оплачивается отдельно",
+                note: "Поможем подобрать оптимальный вариант!"
+                ,
+                serviceMob: "Медицинская страховка\n" +
+                    "\n",
+                costMob: "Оплачивается отдельно: подберем оптимальный вариант!!!\n" +
+                    "\n"
+                ,
+            }
         ]
     }
 ];
@@ -107,7 +197,7 @@ export const otherCountries = [
 const Services = () => {
     const [isMobile, setIsMobile] = useState(false);
     const [showAllCountries, setShowAllCountries] = useState(false);
-    const excludedCountries = ["rabochaya-viza-v-ispaniyu","rabochaya-viza-v-germaniyu", "viza-v-velikobritaniyu", "viza-v-ssha", "viza-v-kitaj", "rabochaya-viza-v-bolgariyu", "rabochaya-viza-v-polshu", "delovaya-viza-v-polshu", "uchebnaya-viza-v-polshu", "gostevaya-polskaya-viza", "viza-v-polsy-po-karte-polyaka"];
+    const excludedCountries = ["rabochaya-viza-v-ispaniyu", "rabochaya-viza-v-germaniyu", "viza-v-velikobritaniyu", "viza-v-ssha", "viza-v-kitaj", "rabochaya-viza-v-bolgariyu", "rabochaya-viza-v-polshu", "delovaya-viza-v-polshu", "uchebnaya-viza-v-polshu", "gostevaya-polskaya-viza", "viza-v-polsy-po-karte-polyaka"];
 
     useEffect(() => {
         const handleResize = () => setIsMobile(window.innerWidth <= 768);
@@ -124,25 +214,25 @@ const Services = () => {
                 {displayedCountries
                     .filter(country => !excludedCountries.includes(country.url))
                     .map((country, index) => (
-                    <Link href={`/shengenskie-vizy/${country.url}`} key={index}>
-                        <div className="bg-white border border-[#ECECEC] rounded-lg lg:rounded-[4px] overflow-hidden shadow-sm cursor-pointer
+                        <Link href={`/shengenskie-vizy/${country.url}`} key={index}>
+                            <div className="bg-white border border-[#ECECEC] rounded-lg lg:rounded-[4px] overflow-hidden shadow-sm cursor-pointer
                             transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg">
-                            <img src={country.img} alt={country.name} className="w-full object-cover"/>
-                            <div className="lg:p-8 md:p-6 sm:p-4 mdd:py-4 mdd:pl-1 mdd:pr-1">
-                                <div className="flex flex-row justify-between items-center">
-                                    <div className="flex sm:gap-2 mdd:gap-0.5 items-center">
-                                        <img src={country.svg} alt={country.name} className="h-6"/>
-                                        <p className="font-medium mdd:text-[16px] dr:text-[14px] sm:text-lg md:text-xl lg:text-xl">
-                                            {country.name}
-                                        </p>
+                                <img src={country.img} alt={country.name} className="w-full object-cover"/>
+                                <div className="lg:p-8 md:p-6 sm:p-4 mdd:py-4 mdd:pl-1 mdd:pr-1">
+                                    <div className="flex flex-row justify-between items-center">
+                                        <div className="flex sm:gap-2 mdd:gap-0.5 items-center">
+                                            <img src={country.svg} alt={country.name} className="h-6"/>
+                                            <p className="font-medium mdd:text-[16px] dr:text-[14px] sm:text-lg md:text-xl lg:text-xl">
+                                                {country.name}
+                                            </p>
+                                        </div>
+                                        <img className="lg:w-6 md:w-6 sm:w-6 mdd:hidden" src="/Line 5.png" alt=""/>
+                                        <img className={"mdd:w-3 dr:w-3 sm:hidden"} src={"/line123.png"} alt={""}/>
                                     </div>
-                                    <img className="lg:w-6 md:w-6 sm:w-6 mdd:hidden" src="/Line 5.png" alt=""/>
-                                    <img className={"mdd:w-3 dr:w-3 sm:hidden"} src={"/line123.png"} alt={""}/>
                                 </div>
                             </div>
-                        </div>
-                    </Link>
-                ))}
+                        </Link>
+                    ))}
             </div>
 
             {isMobile && !showAllCountries && (

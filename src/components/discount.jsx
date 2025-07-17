864704c9a4612f9c5b3bca3d6c7e093c386d2143
ana <yosip19h@gmail.com>
2025-07-17 17:31:52 +0500

    noDiscount

--- src/components/discount.jsx
+++ src/components/discount.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Discount = () => {
     return (
-        <div className="sm:hidden -mt-20 mb-5 px-[7%] bg-orange-500 text-lg font-medium text-white py-1.5 text-center">
-            <p><span className="font-bold text-blue-950 underline">Оформим визу</span> до конца 2025 года или <span className="font-bold text-blue-950 underline">вернем деньги!</span></p>
+        // <div className="sm:hidden -mt-20 mb-5 px-[7%] bg-orange-500 text-lg font-medium text-white py-1.5 text-center">
+        //     <p><span className="font-bold text-blue-950 underline">Оформим визу</span> до конца 2025 года или <span className="font-bold text-blue-950 underline">вернем деньги!</span></p>
+        // </div>
+        <div>
+
         </div>
     );
 };
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -307,9 +307,9 @@ export default function Header() {
                         </div>
                     </div>
                 </div>
-                <div className="mdd:hidden px-[7%] bg-orange-500 text-lg font-medium text-white py-1.5 text-center">
-                    <p><span className="font-bold text-blue-950 underline">Оформим визу</span> до конца 2025 года или <span className="font-bold text-blue-950 underline">вернем деньги!</span></p>
-                </div>
+                {/*<div className="mdd:hidden px-[7%] bg-orange-500 text-lg font-medium text-white py-1.5 text-center">*/}
+                {/*    <p><span className="font-bold text-blue-950 underline">Оформим визу</span> до конца 2025 года или <span className="font-bold text-blue-950 underline">вернем деньги!</span></p>*/}
+                {/*</div>*/}
                 <AnimatePresence>
                     {isMenuOpen && (
                         <>

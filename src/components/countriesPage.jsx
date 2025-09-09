a2132a55c1cee181f9931097e6bd0b51d843051a
Alex <alexastashenok@gmail.com>
2025-09-09 19:05:29 +0300

    fix icons

--- src/components/countriesPage.jsx
+++ src/components/countriesPage.jsx
@@ -724,8 +724,8 @@ export default function CountryPage({ breadcrumbs, countryData, countryUrl }) {
                 {selectedCountry.url === "viza-v-italiyu" && ( 
                     <>
                         <ul className={`lg:absolute 1400m:top-[20%] mxl:top-[40%] xl:top-[30%] 2xl:top-[45%]  lg:right-[7%] lg:transform lg:-translate-y-3 space-y-4 text-left lg:p-4 sm:pt-10 mdd:mt-8 mdd:pb-5 rounded-md pl-[7%] ml-[7%] mb-[20px] mr-auto lg:m-0`}>
-                            <AdvantageItemTop value="от 10 дней" description="срок рассмотрения заявления" iconPath={"/timeicon.svg"} />
-                            <AdvantageItemTop value="до 2-х лет" description="срок действия визы" iconPath={"/docicon.svg"}/>
+                            <AdvantageItemTop value="от 10 дней" description="срок рассмотрения заявления" iconPath={"/icontime.svg"} />
+                            <AdvantageItemTop value="до 2-х лет" description="срок действия визы" iconPath={"/icondoc.svg"}/>
                             <AdvantageItemTop value="99%" description="одобрения виз" iconPath={"/approvalicon.svg"}/>
                         </ul>
                     </>

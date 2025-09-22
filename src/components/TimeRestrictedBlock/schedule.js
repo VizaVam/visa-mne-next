fc9171f55cb8f589153b9df5a26b8f024e88d6f6
Alex <alexastashenok@gmail.com>
2025-09-22 22:35:24 +0300

    adds

---
+++ src/components/TimeRestrictedBlock/schedule.js
@@ -0,0 +1,13 @@
+export const schedule = {
+    weekdays: [
+      { start: 9, end: 12, type: "promo" },
+      { start: 14, end: 14, type: "insurance" },
+      { start: 17, end: 18, type: "insurance" },
+      { start: 20, end: 23, type: "promo" },
+    ],
+    weekends: [
+      { start: 11, end: 12, type: "promo" },
+      { start: 14, end: 15, type: "insurance" },
+      { start: 18, end: 23, type: "promo" },
+    ],
+  };

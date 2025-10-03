adddde3f4b4749c44000d87701d53713853dcd72
Alex <alexastashenok@gmail.com>
2025-10-03 17:34:58 +0300

    add picture

--- src/components/topic8/topic8.jsx
+++ src/components/topic8/topic8.jsx
@@ -166,7 +166,7 @@ export default function OnasPage({breadcrumbs}) {
                 {/* Banner Section */}
                 <div className="w-full relative">
                     <Image
-                        src="/topic7.jpg"
+                        src="/topic8.jpg"
                         alt="О компании VisaVam"
                         width={1600}
                         height={1000}
@@ -174,7 +174,7 @@ export default function OnasPage({breadcrumbs}) {
                         className="w-full h-96 object-cover px-0 lg:px-[7%] md:px-[7%] mdd:hidden"
                     />
                     <Image
-                        src="/topic7.jpg"
+                        src="/topic8.jpg"
                         alt="О компании VisaVam"
                         width={600}
                         height={400}

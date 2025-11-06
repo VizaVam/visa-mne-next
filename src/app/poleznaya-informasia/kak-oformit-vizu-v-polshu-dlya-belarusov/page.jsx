dfbaa01240aced9be02a91c408532d84d555519d
Alex <alexastashenok@gmail.com>
2025-11-06 22:14:18 +0300

    changes for topics

--- src/app/poleznaya-informasia/kak-oformit-vizu-v-polshu-dlya-belarusov/page.jsx
+++ src/app/poleznaya-informasia/kak-oformit-vizu-v-polshu-dlya-belarusov/page.jsx
@@ -4,6 +4,9 @@ import Kakof from "@/components/kakof";
 export const metadata = {
     title: "Как оформить виза в Польшу для белорусов —  документы, стоимость, нюансы",
     description: "Как получить визу в Польшу для белорусов: список документов, стоимость, сроки, адреса визовых центров и агентств. Полное руководство от экспертов визового агентства Visa Vam.",
+    other: {
+        lastModified: "2025-10-29T14:32:00+03:00",
+    },
     alternates: {
         canonical: "https://visavampro.by/poleznaya-informasia/kak-oformit-vizu-v-polshu-dlya-belarusov",
     },

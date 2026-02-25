db77713f5f06a679a55ec26375537f798a5be6a5
VizaVam <info.visavam@gmail.com>
2026-02-25 22:01:20 +0300

    fix bx24 loader

--- src/app/layout.js
+++ src/app/layout.js
@@ -115,7 +115,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${interSans.variable} ${caveatSans.variable} antialiased`}>
         <GoogleTagManager />
-        <script data-b24-form="click/507/hhzsmd"
+        <script data-b24-form="click/507/hhzsmd" strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.by/b27090570/crm/form/loader_507.js');
